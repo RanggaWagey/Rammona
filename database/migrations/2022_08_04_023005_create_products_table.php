ca7d14e07b14e1f32f16c74c4345e65bfc378013
unknown <rwigey@gmail.com>
2022-10-14 16:57:38 +0700

    migration update

---
+++ database/migrations/2022_08_04_023005_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->double('price');
+            $table->string('image')->nullable();
+            $table->timestamps();
+            $table->foreignId('sub_category_id')->references('id')->on('sub_categories')->onDelete('cascade');
+        });
+    }
+
+
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
